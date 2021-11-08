64ca424634f6f134a40d1afa7d600be1e0d42c04
anthatip <anthatip@cisco.com>
2021-11-08 11:02:52 +0000

    assignment of testng

---
+++ src/pages/LoginPage.java
@@ -0,0 +1,67 @@
+
+package pages;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.asserts.SoftAssert;
+
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import test.BaseClass;
+
+public class LoginPage {
+
+
+WebDriver driver= BaseClass.driver;
+ExtentTest test = BaseClass.test;
+
+@FindBy(id="user-name")
+WebElement username;
+
+@FindBy(id="password")
+WebElement password;
+
+@FindBy(id="login-button")
+WebElement login;
+
+@FindBy(xpath="//button[@class='error-button']")
+WebElement Error;
+public LoginPage() {
+PageFactory.initElements(driver, this);
+}
+
+
+
+public void Login(String UserName,String Password) {
+
+	WebDriverWait wait=new  WebDriverWait(driver,30);
+    //wait.until(ExpectedConditions.visibilityOf(LoginLink));
+
+username.sendKeys(UserName);
+test.log(LogStatus.PASS,"Enter username"+UserName,"Successfully enter the name");
+password.sendKeys(Password);
+test.log(LogStatus.PASS,"Enter password"+Password,"Successfully enter the password");
+login.click();
+test.log(LogStatus.PASS,"click login button","Successfully clicked the button");
+
+}
+
+public void ErrorCheck() {
+
+
+String Actualmsg = Error.getText();
+String Expmsg = "Epic sadface: Username and password do not match any user in this service";
+
+//Assert.assertEquals(Actualmsg, Expmsg);
+SoftAssert soft = new SoftAssert();
+soft.assertEquals(Actualmsg, Expmsg);
+//soft.assertAll();
+}
+
+}
